44110ddd28ad4249eafe882e5245c9292e184de3
NguyenLam1708 <lam170804@gmail.com>
2024-10-04 15:00:51 +0700

    create property deleteAt

--- src/main/java/org/example/nishiki_koi_shop/model/entity/FishType.java
+++ src/main/java/org/example/nishiki_koi_shop/model/entity/FishType.java
@@ -3,6 +3,7 @@ package org.example.nishiki_koi_shop.model.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
@@ -19,6 +20,7 @@ public class FishType {
 
     private String name;
     private String description;
+    private LocalDate deletedAt;
 
     @OneToMany(mappedBy = "fishType")
     private List<Fish> fish;
